291296510e1a236901d266f22ff16955ad0a5d3e
Jesús Fernando Capriel Tzarax <jesustzarax999@hotmail.com>
2020-05-11 20:23:55 -0600

    Creacion Clases

---
+++ Departamentos.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace examenParcial1
+{
+    class Departamentos
+    {
+        string codigo;
+        string departamento;
+
+        public string Codigo { get => codigo; set => codigo = value; }
+        public string Departamento { get => departamento; set => departamento = value; }
+    }
+}
---
+++ Registros.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace examenParcial1
+{
+    class Registros
+    {
+        string codigo;
+        List<string> medicion;
+        List<string> fechaMedicion;
+
+        public string Codigo { get => codigo; set => codigo = value; }
+        public List<string> Medicion { get => medicion; set => medicion = value; }
+        public List<string> FechaMedicion { get => fechaMedicion; set => fechaMedicion = value; }
+
+        public Registros()
+        {
+            Codigo = "";
+            Medicion = new List<string>();
+            FechaMedicion = new List<string>();
+        }
+    }
+}
